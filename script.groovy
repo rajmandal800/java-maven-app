57ba053760de12377e9822c8fece22bf559b276e
rajmandal800 <rajmandal800@gmail.com>
2025-08-01 12:03:36 +0200

    added user input in deploy stage

--- script.groovy
+++ script.groovy
@@ -10,9 +10,9 @@ def buildImage() {
         // sh "echo $PASS | docker login -u $USER --password-stdin"
         // sh 'docker push nanajanashia/demo-app:jma-2.0'
 
-        sh 'docker build -t 192.168.64.8:8083/java-maven-app:jma-1.2.0 .'
+        sh "docker build -t 192.168.64.8:8083/java-maven-app:jma-${params.VERSION} ."
         sh "echo $PASS | docker login -u $USER --password-stdin 192.168.64.8:8083"
-        sh 'docker push 192.168.64.8:8083/java-maven-app:jma-1.2.0'
+        sh "docker push 192.168.64.8:8083/java-maven-app:jma-${params.VERSION}"
     }
 } 
 def runTests() {
